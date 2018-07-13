d3897249ada8680d639fa4496312add7bc4c0e9b
KrazyCo <38669844+KrazyCo@users.noreply.github.com>
2018-07-13 19:42:02 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -144,7 +144,7 @@ async def on_message(message):
 
         if message.content.upper().startswith(prefix + "HELP"):
             await client.send_message(message.author, embed=embed)
-            dm = discord.Embed(description=":mailbox_with_mail: Check DM's", colour=0x1E894A)
+            dm = discord.Embed(title=":mailbox_with_mail: Check DM's", colour=0x1E894A)
             await client.send_message(message.channel, embed=dm)
             print ("help")
         if message.content.upper().startswith(prefix + "INFO"):
